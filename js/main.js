e129a4362845a9f10f46173009e65a0a3364b12b
hamzazirari <hamzazirari6002@gmail.com>
2025-10-31 11:31:13 +0100

    refactor: code improvement features

--- js/main.js
+++ js/main.js
@@ -7,12 +7,13 @@ const header = document.querySelector("header");
 
 
 btnPlus.addEventListener("click", function() {
-    maModale.classList.remove("d-none");
-    pageContent.classList.add("backdrop-blur");
-    header.classList.add("backdrop-blur");
+    maModale.classList.remove("d-none");   
+    pageContent.classList.add("blur-bg"); 
+    header.classList.add("blur-bg"); 
 });
-fermerModale.addEventListener("click",function() {
-    maModale.classList.add("d-none");
-    pageContent.classList.remove("backdrop-blur");
-    header.classList.remove("backdrop-blur");
+
+fermerModale.addEventListener("click", function() {
+    maModale.classList.add("d-none");      
+    pageContent.classList.remove("blur-bg"); 
+    header.classList.remove("blur-bg"); 
 });
